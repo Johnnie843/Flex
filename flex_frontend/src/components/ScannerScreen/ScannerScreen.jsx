74e246b42c8ef920f77ba42fe39c5e45db1e9cc9
Cooper <johncoo@americas.corpdir.net>
2020-06-25 14:53:27 -0400

    updating project

--- flex_frontend/src/components/ScannerScreen/ScannerScreen.jsx
+++ flex_frontend/src/components/ScannerScreen/ScannerScreen.jsx
@@ -4,27 +4,53 @@ import Card from "../Card/Card.jsx";
 import SequenceTable from "../SequenceTable/SequenceTable.jsx";
 import ScannerInput from "./ScannerInput.jsx" 
 import UserCard from "../UserCard/UserCard.jsx";
+import VehicleCard from "../VehicleCard/VehicleCard.jsx";
 
 class ScannerScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gepard_engine_sequence: [{"planName":"ASCLMA11_INT01_CSQ03","sequenceNumber":13353380,"lastProgressChange":"3/11/20 10:42 AM","productionOrder":"","pnrCheck":"","blockingStatus":"0","productionProgress":"AVAILABLE","modelType":"","taktName":null,"vin":"","ls1":"9147","created":null,"createdBy":null,"changed":null,"changedBy":null,"codes":[]},],
+      next_engine_in_sequence: 1
+    }
+  }
+
+  componentDidMount() {
+
+    fetch('http://localhost:8000/gepard/enginesequence')
+    .then((response) => response.json())
+    .then(sequence => {
+        this.setState({ gepard_engine_sequence: sequence.sequencePlans});
+        fetch('http://53.234.70.143:81/gepard/qsys/smotor/?format=json&spnr8='+this.state.gepard_engine_sequence[0].productionOrder+this.state.gepard_engine_sequence[0].pnrCheck)
+        .then((response) => response.json())
+        .then(engine_info => {
+              this.setState({ next_engine_in_sequence: engine_info[0].smotor_no});
+           })
+    })
+
+    
+  }
+
 
   render() {
 
     return (
       
       <div className="content">
-                  <div class = "container">
+                  <div className = "container">
                     <Col>
-                      <Row>
-                      <Card title="Next Engine In Sequence"
-                            category={""}
+                      <VehicleCard production_number={this.state.gepard_engine_sequence[0].productionOrder}
+                            category="Employee Related Alerts"
+                            stats="Updated 3 minutes ago"
+                            vin={this.state.gepard_engine_sequence[0].vin}
+                            build={this.state.next_engine_in_sequence}
+                            info={this.state.gepard_engine_sequence[0].modelType}
+                            statsIcon="fa fa-history"
                             content={
-                              <UserCard></UserCard>
-                            }
-                            >         
-                        </Card>
-
-                        <ScannerInput></ScannerInput>
-                        </Row>
+                                      <div></div>
+                                     }>              
+                      </VehicleCard>
+                      <ScannerInput></ScannerInput>
                     </Col>
                   </div>                   
         </div>
---
+++ flex_frontend/src/components/VehicleCard/VehicleCard.jsx
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import Button from "../CustomButton/CustomButton.jsx";
+
+import {
+  Row,
+  Col,
+} from "react-bootstrap";
+
+export class VehicleCard extends Component {
+  render() {
+    return (
+      <div className={"card"}>
+        <div className={"vehicle_card_header text-center"}>
+          <Row>
+            <div className={"next_order"}> 
+            <Col md={2}>
+            <h4 className="vehicle_card_title">Next Order In Sequence</h4>
+            <p className="production_number">{this.props.production_number}</p>
+            </Col>
+            </div>
+            <Col md={1}>
+              <Row>
+                <div className="text-left">
+                    <i className="vehicle_card_icons material-icons">directions_bus</i>
+                    <p className="vehicle_card_data">{this.props.vin}</p>
+                </div>
+              </Row>
+              <hr/>
+              <Row>
+                <div className="text-left">
+                    <i className="vehicle_card_icons material-icons">info</i>
+                    <p className="vehicle_card_data">{this.props.info}</p>
+                </div>
+              </Row>
+              <hr/>
+              <Row>
+                <div className="text-left">
+                    <i className="vehicle_card_icons material-icons">build</i>
+                    <p className="vehicle_card_data">{this.props.build}</p>
+                </div>
+              </Row>
+              
+            </Col>
+          </Row>
+        </div>
+        <div
+          className={
+            "content" +
+            (this.props.ctAllIcons ? " all-icons" : "") +
+            (this.props.ctTableFullWidth ? " table-full-width" : "") +
+            (this.props.ctTableResponsive ? " table-responsive" : "") +
+            (this.props.ctTableUpgrade ? " table-upgrade" : "")
+          }
+        >
+          {this.props.content}
+
+          <div className="footer">
+            {this.props.legend}
+            {this.props.stats != null ? <hr /> : ""}
+            <div className="stats">
+              <i className={this.props.statsIcon} /> {this.props.stats}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default VehicleCard;
